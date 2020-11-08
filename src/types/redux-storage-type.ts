d0199bbdc17c75022ccee4c2097b8bdf0e509a16
Сафарян Павел Ширакович <safaryan.pavel@wildberries.ru>
2020-11-08 19:03:53 +0300

    configs

--- src/types/redux-storage-type.ts
+++ src/types/redux-storage-type.ts
@@ -1,5 +1,5 @@
-import {PLAY_LIST_REDUCER_NAME} from "../player/redux/playlist/reducer";
-import {PlayListStorageType} from "../player/redux/playlist/types";
+import {PLAY_LIST_REDUCER_NAME} from "../player/redux/player/reducer";
+import {PlayListStorageType} from "../player/redux/player/types";
 
 export type ReduxStorageType = {
   [PLAY_LIST_REDUCER_NAME]: PlayListStorageType;
